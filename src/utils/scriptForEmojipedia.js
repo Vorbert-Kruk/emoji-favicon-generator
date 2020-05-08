09d5a8ad54e2c4bb66a0c7d2c4473e8a6c12ec74
Vorbert Kruk <pryrokopter@gmail.com>
2020-05-08 19:39:01 +0200

    Updated the scraping method

--- src/utils/scriptForEmojipedia.js
+++ src/utils/scriptForEmojipedia.js
@@ -1,18 +1,18 @@
-const heh = {};
+const emojis = {};
 
 [...document.querySelectorAll('.emoji-list li')].forEach(emojiItem => {
   const getObjectName = name => name[0].toLowerCase() + name.slice(1);
 
-  const itemContent = emojiItem.innerText.split(' ');
-  const emojiFace = itemContent[0];
-  itemContent.shift();
+  const itemTextContent = emojiItem.innerText.split(' ');
+  const emojiFace = itemTextContent[0];
+  itemTextContent.shift();
   const emojiName = getObjectName(
-    itemContent
+    itemTextContent
       .join('')
       .replace(/[-,:’.()!“”]/g, '')
       .replace(/[&]/, 'And')
   );
-  heh[emojiName] = emojiFace;
+  emojis[emojiName] = emojiFace;
 });
 
-document.body.innerHTML = JSON.stringify(heh);
+document.body.innerHTML = JSON.stringify(emojis);
