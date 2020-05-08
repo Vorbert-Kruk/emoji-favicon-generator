a19e2c5f2ee91f47abd1083a131e75046ad9f98a
Vorbert Kruk <pryrokopter@gmail.com>
2020-05-08 16:54:34 +0200

    Changed theme variables

--- src/components/organisms/Hero.jsx
+++ src/components/organisms/Hero.jsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 const Hero = styled.button`
   background-color: #77f;
-  font-size: 16px;
+  color: ${({ theme }) => theme.accentOpaque};
   padding: 0.35em 0.5em;
 `;
 
