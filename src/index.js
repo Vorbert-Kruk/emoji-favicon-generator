2ff2d441f67537ade7518030420165c7919300c3
Vorbert Kruk <pryrokopter@gmail.com>
2020-05-11 19:13:32 +0200

    Added react redux to the project

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'styles/main.scss';
+import store from 'store/store';
+import { Provider } from 'react-redux';
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-import 'styles/main.scss';
-
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/store/initialState.js
@@ -0,0 +1,5 @@
+const initialState = {
+  pickedEmoji: undefined
+};
+
+export default initialState;
