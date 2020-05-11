9d9a18a409272f492b717229afe48b25f62e67aa
Vorbert Kruk <pryrokopter@gmail.com>
2020-05-11 18:59:08 +0200

    Added emoji popup

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 import withTheme from 'theme/withTheme';
 import HomePage from 'views/HomePage';
+import EmojiPopup from 'components/organisms/EmojiPopup';
 
 function App() {
   return (
     <div className="App">
       <HomePage />
+      <EmojiPopup />
     </div>
   );
 }
---
+++ src/components/organisms/EmojiPopup.jsx
@@ -0,0 +1,41 @@
+import React, { memo } from 'react';
+import styled from 'styled-components';
+import Emoji from 'a11y-react-emoji';
+
+const StyledWrapper = styled.div`
+  position: fixed;
+  display: flex;
+  bottom: 1.25rem;
+  right: 1.25rem;
+  padding: 1rem 1.25rem;
+  border-radius: 0.5rem;
+  background-color: ${({ theme }) => theme.accent};
+  flex-direction: column;
+  align-items: center;
+`;
+
+const StyledHeader = styled.h3`
+  font-size: 2.25rem;
+  text-align: center;
+`;
+
+const StyledEmojiWrapper = styled.div`
+  margin-top: 0.75rem;
+  padding: 1rem 1.25rem;
+  font-size: 4rem;
+  background-color: ${({ theme }) => theme.secondary};
+  border-radius: 0.75rem;
+`;
+
+const EmojiPopup = () => {
+  return (
+    <StyledWrapper>
+      <StyledHeader>Picked emoji:</StyledHeader>
+      <StyledEmojiWrapper>
+        <Emoji symbol={'😀'} />
+      </StyledEmojiWrapper>
+    </StyledWrapper>
+  );
+};
+
+export default memo(EmojiPopup);
