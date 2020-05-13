5e9299761f3b728f4390957d581709f65f66f366
Vorbert Kruk <pryrokopter@gmail.com>
2020-05-13 10:45:57 +0200

    Added appear animation to the emoji sections

--- src/components/organisms/EmojisSections.jsx
+++ src/components/organisms/EmojisSections.jsx
@@ -1,13 +1,26 @@
 import React, { memo } from 'react';
 import emojiCategories from 'utils/emojiCategories';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
 import EmojiSection from 'components/molecules/EmojiSection';
+
+const appearAnimation = keyframes`
+  0% {
+    transform: translateY(-1.5rem);
+    opacity: 0;
+  }
+
+  100% {
+    transform: translateY(0);
+    opacity: 1;
+  }
+`;
 
 const StyledWrapper = styled.div`
   max-width: ${({ theme }) => theme.maxContentWidth};
   margin: 0 auto;
   padding-bottom: 2.25rem;
+  animation: ${appearAnimation} 750ms ease forwards;
 
   @media screen and (max-width: ${({ theme }) => theme.maxContentWidth}) {
     padding: 0 3%;
